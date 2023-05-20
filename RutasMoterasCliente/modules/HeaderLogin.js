f438690800cec6477877380290efdc0a6d26d5df
Ivan <ivanca2003@gmail.com>
2023-05-20 19:02:52 +0200

    Alineamiento de header con y sin flecha

--- RutasMoterasCliente/modules/HeaderLogin.js
+++ RutasMoterasCliente/modules/HeaderLogin.js
@@ -1,8 +1,12 @@
 const Header={
     template:`
-        <header :style="header">
+        <header :style="header" v-if="flecha !== true">
             <img v-if="flecha !== true" src="imgs/flecha.svg" :style="img" @click="$emit('inicio')">
             <h1 :style="h1" @click="$emit('inicio')">RutasMoteras</h1>
+            <a :style="a" @click="$emit('login')">Login</a>
+        </header>
+        <header :style="header" v-if="flecha === true">
+            <h1 :style="h1derecha" @click="$emit('inicio')">RutasMoteras</h1>
             <a :style="a" @click="$emit('login')">Login</a>
         </header>
     `,
@@ -19,20 +23,22 @@ const Header={
                 img:{
                     "max-width": "50px",
                     "margin-left": "150px",
+                    "margin-right": "-10px",
                 },
                 h1:{
                     "font-family": "Roboto",
                     "flex": "1",
                     "text-align": "center",
                 },
+                h1derecha: {
+                    "font-family": "Roboto",
+                    "flex": "1",
+                    "text-align": "center",
+                    "margin-left": "190px",
+                },
                 a:{
                     "text-align": "right",
                     "margin-right": "150px",
-                },
-                user:{
-                    "text-align": "right",
-                    "margin-right": "50px",
-                    "margin-left": "-250px",
                 },
             }
         },
--- RutasMoterasCliente/modules/HeaderLogout.js
+++ RutasMoterasCliente/modules/HeaderLogout.js
@@ -1,8 +1,13 @@
 const Header={
     template:`
-        <header :style="header">
-            <img v-if="flecha !== true" src="imgs/flecha.svg" :style="img" @click="$emit('inicio')">
+        <header :style="header" v-if="flecha !== true">
+            <img src="imgs/flecha.svg" :style="img" @click="$emit('inicio')">
             <h1 :style="h1" @click="$emit('inicio')">RutasMoteras</h1>
+            <a :style="user" @click="$emit('usuario')" @click="$emit('rutas')">Usuario</a>
+            <a :style="a" @click="$emit('logout')">Logout</a>
+        </header>
+        <header :style="header" v-if="flecha === true">
+            <h1 :style="h1derecha" @click="$emit('inicio')">RutasMoteras</h1>
             <a :style="user" @click="$emit('usuario')" @click="$emit('rutas')">Usuario</a>
             <a :style="a" @click="$emit('logout')">Logout</a>
         </header>
@@ -20,12 +25,19 @@ const Header={
             img:{
                 "max-width": "50px",
                 "margin-left": "150px",
-                "margin-right": "-140px",
+                "margin-right": "-170px",
             },
             h1:{
                 "font-family": "Roboto",
                 "flex": "1",
                 "text-align": "center",
+                "margin-left": "18px",
+            },
+            h1derecha: {
+                "font-family": "Roboto",
+                "flex": "1",
+                "text-align": "center",
+                "margin-left": "48px",
             },
             a:{
                 "text-align": "right",
