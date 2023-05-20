3c10ff47ec8c085ae36250c8eb331d9808ddb613
Ivan <ivanca2003@gmail.com>
2023-05-20 21:04:22 +0200

    Comprobación de email

--- RutasMoterasCliente/modules/SingIn.js
+++ RutasMoterasCliente/modules/SingIn.js
@@ -4,11 +4,11 @@ const SingIn={
             <p>Sing In</p>
             <input type="text" v-model="username" placeholder="Nombre de usuario">
             <input type="email" v-model="email" placeholder="Correo electrónico">
+            <div class="error" v-if="erroremail">{{ erroremail }}</div>
             <input type="password" v-model="password" placeholder="Contraseña">
             <button type="submit" :style="button">Iniciar sesión</button>
             <div class="error" v-if="error">{{ error }}</div>
             <p>Ya tienes una cuenta registrada? <a @click="$emit('login')">Iniciar sesión.</a></p>
-
         </form>
     `,
 
@@ -18,6 +18,7 @@ const SingIn={
             email: '',
             password: '',
             error: '',
+            erroremail: '',
             div:{
                 "padding-top": "150px",
             },
@@ -32,22 +33,32 @@ const SingIn={
         }
     },
 
-    methods:{
-        // Sirve para crear el usuario en la API
+    methods: {
         singin() {
-            axios.post('http://127.0.0.1/api/usuarios_detail/', {   'username': this.username, 
-                                                                    'email': this.email, 
-                                                                    'password': this.password, 
-                })
-                .then(response => {
-                    this.error = '';
-                    this.$emit("MostrarLogin");
-                })
-                .catch(error => {
-                    this.error = 'Ocurrió algo inesperado, vuelve a intentarlo.'
-                });
-        }
-    },
+            if (!this.validateEmail(this.email)) {
+                this.error = 'El correo electrónico no es válido.';
+                return;
+            }
+        
+            axios.post('http://127.0.0.1/api/usuarios_detail/', { username: this.username,
+                                                                email: this.email,
+                                                                password: this.password,
+            })
+            .then(response => {
+                this.error = '';
+                this.$emit('MostrarLogin');
+            })
+            .catch(error => {
+                this.error = 'Ocurrió algo inesperado, vuelve a intentarlo.';
+            });
+        },
+        
+        validateEmail(email) {
+          const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+          return emailRegex.test(email);
+        },
+      },
+    
 }
 
 export default SingIn;
