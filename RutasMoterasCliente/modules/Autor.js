b2faa711663bfb325b25fe6257ff92b0b80f9328
Ivan <ivanca2003@gmail.com>
2023-06-11 12:26:56 +0200

    Enlaces

--- RutasMoterasCliente/modules/Autor.js
+++ RutasMoterasCliente/modules/Autor.js
@@ -11,7 +11,7 @@ const Autor={
             <p :style="info">No importa si eres un novato en el mundo de las motos o un veterano experimentado, en RutasMoteras todos son bienvenidos. Únete a nosotros y embárcate en una aventura llena de emoción, camaradería y descubrimiento. ¡Prepárate para explorar el mundo sobre dos ruedas!</p>
             <!-- Licencia de la web -->
             <div :style="div2">
-                <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img :style="img" alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.
+                <a target="_blank" rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img :style="img" alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" target="_blank" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.
             </div>
         </div>
         `,
